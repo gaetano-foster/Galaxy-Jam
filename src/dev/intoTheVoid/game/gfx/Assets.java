bd8d271f42e8c51f4aea3b6c44c0368c5be03a0d
intoTheVoid385 <gaetanox109@gmail.com>
2021-09-04 11:19:55 -0400

    added spritesheet class and some player sprites

--- src/dev/intoTheVoid/game/gfx/Assets.java
+++ src/dev/intoTheVoid/game/gfx/Assets.java
@@ -6,6 +6,8 @@ import java.util.HashMap;
 public class Assets
 {
     private HashMap<String, BufferedImage> sprites = new HashMap<String, BufferedImage>(); // easy access to all sprites
+    private SpriteSheet sheet;
+    private final int DEF_SIZE = 32;
 
     /*
       * Loads all sprites into memory.
@@ -17,7 +19,20 @@ public class Assets
      */
     public void loadAssets()
     {
+        sheet = new SpriteSheet("/textures/spriteSheet.png");
 
+        // sprites for while player is alive
+        sprites.put("player00", sheet.crop(0, 0, 1, 1, DEF_SIZE));
+        sprites.put("player01", sheet.crop(1, 0, 1, 1, DEF_SIZE));
+        sprites.put("player02", sheet.crop(2, 0, 1, 1, DEF_SIZE));
+
+        // sprites for dying player
+        sprites.put("player10", sheet.crop(0, 1, 1, 1, DEF_SIZE));
+        sprites.put("player11", sheet.crop(1, 1, 1, 1, DEF_SIZE));
+        sprites.put("player12", sheet.crop(2, 1, 1, 1, DEF_SIZE));
+        sprites.put("player13", sheet.crop(0, 2, 1, 1, DEF_SIZE));
+        sprites.put("player14", sheet.crop(1, 2, 1, 1, DEF_SIZE));
+        sprites.put("player15", sheet.crop(2, 2, 1, 1, DEF_SIZE));
     }
 
     public BufferedImage getSprite(String key)
---
+++ src/dev/intoTheVoid/game/gfx/SpriteSheet.java
@@ -0,0 +1,23 @@
+package dev.intoTheVoid.game.gfx;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet
+{
+    private BufferedImage img; // the spritesheet itself
+
+    public SpriteSheet(String path)
+    {
+        img = FileLoader.loadImage(path); // simple
+    }
+
+    public BufferedImage crop(int x, int y, int width, int height, int def) // for getting sprites that are default size
+    {
+        return img.getSubimage(x * def, y * def, width * def, height * def);
+    }
+
+    public BufferedImage cropSpecific(int x, int y, int width, int height) // for getting sprites with more specific dimensions
+    {
+        return this.crop(x, y, width, height, 1); // ezpz
+    }
+}
