5b08d15ebd81f28dd140dbf70fc8885a644bc9ff
Gaetano Foster <gaetanox109@gmail.com>
2023-09-10 20:21:18 -0400

    Fixed the screen to the correct height.

--- src/dev/intoTheVoid/game/Launcher.java
+++ src/dev/intoTheVoid/game/Launcher.java
@@ -4,7 +4,7 @@ import dev.intoTheVoid.game.io.Display;
 
 public class Launcher {
     public static void main(String args[]) {
-        Game game = new Game(600, 700, "Galaxy Jam"); // I like to think the low resolution will give the "1980's arcade" vibe
+        Game game = new Game(600, 800, "Galaxy Jam");
         game.start();
     }
 }
