4ab5b3c89e1a08a58ed37caace789b8765b27e07
Gaetano Foster <gaetanox109@gmail.com>
2023-09-10 20:28:14 -0400

    New constructor which allows for a different sprite, and a different rendering routine depending on the speed of the Meteor.

--- src/dev/intoTheVoid/game/entities/enemies/Meteor.java
+++ src/dev/intoTheVoid/game/entities/enemies/Meteor.java
@@ -2,17 +2,36 @@ package dev.intoTheVoid.game.entities.enemies;
 
 import dev.intoTheVoid.game.Game;
 import dev.intoTheVoid.game.entities.projectiles.Projectile;
+import dev.intoTheVoid.game.gfx.Assets;
+import dev.intoTheVoid.game.sfx.SoundPlayer;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.nio.Buffer;
 
 // falling projectile
 public class Meteor extends Projectile {
-    public Meteor(float x, float y, Game game) {
-        super(game.getAssets().getSprite("projectile2"), x, y, defaultSize, defaultSize, game, 8, 1);
+
+    private Assets assets;
+    private boolean meteor = true;
+
+    public Meteor(float x, float y, int speed, Game game) {
+        super(game.getAssets().getSprite("projectile2"), x, y, defaultSize, defaultSize, game, speed, 1);
         id = "mProj";
         bounds.x = 20;
         bounds.y = 15;
         bounds.width *= 2;
+        assets = game.getAssets();
+    }
+
+    public Meteor(float x, float y, int speed, Game game, BufferedImage sprite) {
+        super(sprite, x, y, defaultSize, defaultSize, game, speed, 1);
+        id = "mProj";
+        bounds.x = 20;
+        bounds.y = 15;
+        bounds.width *= 2;
+        assets = game.getAssets();
+        meteor = false;
     }
 
     @Override
@@ -22,11 +41,25 @@ public class Meteor extends Projectile {
 
     @Override
     public void render(Graphics g) {
-        super.render(g);
+        if (!active)
+            return;
+        if (meteor) {
+            if (getSpeed() > 0) {
+                g.drawImage(getSprite(), (int) x, (int) y, (int) width, (int) height, null);
+            } else if (getSpeed() == 0) {
+                g.drawImage(assets.getSprite("projectile3"), (int) x, (int) y, (int) width, (int) height, null);
+            }
+            if (getSpeed() < 0) {
+                g.drawImage(assets.getSprite("projectile4"), (int) x, (int) y, (int) width, (int) height, null);
+            }
+        }
+        else {
+            g.drawImage(getSprite(), (int) x, (int) y, (int) width, (int) height, null);
+        }
     }
 
     @Override
     public void die() {
-
+        super.die();
     }
 }
