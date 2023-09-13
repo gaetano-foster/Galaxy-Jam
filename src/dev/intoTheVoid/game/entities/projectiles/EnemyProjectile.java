2a0b2199020747759e6e6ce8427670c384be2a48
Gaetano Foster <gaetanox109@gmail.com>
2023-09-13 19:27:36 -0400

    EnemyProjectile speed is now a static variable.

--- src/dev/intoTheVoid/game/entities/projectiles/EnemyProjectile.java
+++ src/dev/intoTheVoid/game/entities/projectiles/EnemyProjectile.java
@@ -3,8 +3,10 @@ package dev.intoTheVoid.game.entities.projectiles;
 import dev.intoTheVoid.game.Game;
 
 public class EnemyProjectile extends Projectile {
+    public static final int SPEED = 12;
+
     public EnemyProjectile(float x, float y, float width, float height, Game game) {
-        super(game.getAssets().getSprite("projectile1"), x, y, width, height, game, 12, 1 /* down */);
+        super(game.getAssets().getSprite("projectile1"), x, y, width, height, game, SPEED, 1 /* down */);
         id = "eProj";
     }
 }
